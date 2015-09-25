62029dc9e2a417ecb1c16d2a4d006fcfa674464c
guzhaolun <1120742987@qq.com>
2015-09-25 23:04:49 +0800

    update 198

--- LeetCode/MyCode/198. house_robber.h
+++ LeetCode/MyCode/198. house_robber.h
@@ -1,11 +1,25 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution198
 {
 public:
+	//dp,当前房子抢或者不抢。
 	int rob(vector<int>& nums)
 	{
-
+		int size = nums.size();
+		vector<int> dp(size, 0);
+		if (size == 0)
+			return 0;
+		if (size == 1)
+			return nums[0];
+		dp[0] = nums[0];
+		dp[1] = max(nums[0], nums[1]);
+		for (int i = 2; i < size; i++)
+		{
+			dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+		}
+		return dp[size - 1];
 	}
 };
